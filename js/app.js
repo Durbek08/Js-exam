7a942af4faefabdadc5854efb002f82c86bd14eb
Muhammadxusan08 <fotbol100k@gmail.com>
2024-04-11 14:40:44 +0300

    push

---
+++ js/app.js
@@ -0,0 +1,10 @@
+let nikk = document.querySelectorAll('.nik')
+
+nikk.forEach(function (item , index){
+    item.addEventListener('click' , function(){
+        nikk.forEach(function (element){
+            element.classList.remove('active')
+        })
+        item.classList.add('active')
+    })
+})
