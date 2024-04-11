24a127fcd10aaf05047c45282e0f7997bfedb5ab
Muhammadxusan08 <fotbol100k@gmail.com>
2024-04-11 15:09:04 +0300

    sssss

--- js/app.js
+++ js/app.js
@@ -1,10 +1,10 @@
-let nikk = document.querySelectorAll('.nik')
+let nikk = document.querySelectorAll('nik')
 
 nikk.forEach(function (item , index){
-    item.addEventListener('click' , function(){
-        nikk.forEach(function (element){
-            element.classList.remove('active')
-        })
-        item.classList.add('active')
+   item.addEventListener('click' , function(){
+    nikk.forEach(function (element){
+        element.classList.remove('active')
     })
+    item.classList.add('active')
+   })
 })
