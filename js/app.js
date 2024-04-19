4ddc866dc9548ad9aa6bb9471103c27ab62b4d1b
Muhammadxusan08 <fotbol100k@gmail.com>
2024-04-19 18:49:05 +0300

    push

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let bosilish = document.querySelector('.bosilish')
+let qaytarilish = document.querySelector('.qaytarilish')
+
+bosilish.addEventListener('click' , function(){
+    qaytarilish.classList.toggle('active')
+})
